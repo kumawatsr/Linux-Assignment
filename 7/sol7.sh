a40511de09ef9fa350862c19b7c8286c7ce74d1e
Ram Kumawat <99554928+kumawatsr@users.noreply.github.com>
2023-06-26 14:35:21 +0530

    Update sol7.sh

--- 7/sol7.sh
+++ 7/sol7.sh
@@ -1,9 +1,1 @@
-#!/usr/bin/bash
-
-# Identify processes running for more than 3 days
-processes=$(ps -eo pid,etimes --sort=etimes | awk -v limit=$((3*24*60*60)) '$2 > limit' | awk '{print $1}')
-
-# Terminating the selected processes
-for pid in $processes; do
-    kill "$pid"
-done
+#temp.sh
